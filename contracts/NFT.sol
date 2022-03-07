e4db04aacda22342a491c87d2c15cf6d026f5da2
rajkharvar <rajkharvar1808@gmail.com>
2022-03-07 23:55:27 +0530

    Contract update

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,6 +8,32 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract NFT is ERC721URIStorage, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter public _tokenIds;
+  mapping(uint => bool) public NFTsSold;
+  uint public listingPrice  = 0.1 ether;
 
   constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+  function setListingPrice(uint _listingPrice) public onlyOwner {
+    listingPrice = _listingPrice;
+  }
+
+  function mintNFT(string memory _tokenURI) public onlyOwner {
+    _tokenIds.increment();
+
+    uint256 newTokenId = _tokenIds.current();
+    _mint(msg.sender, newTokenId);
+    _setTokenURI(newTokenId, _tokenURI);
+  }
+
+  function transferNFT(uint _tokenId) public payable {
+    require(!NFTsSold[_tokenId], "This NFT is already sold");
+    require(msg.value == listingPrice, "Price must be equal to listing price");
+
+    NFTsSold[_tokenId] = true;
+    safeTransferFrom(address(this), msg.sender, _tokenId);
+  }
+  
+  function withdraw() public onlyOwner {
+    payable(owner()).transfer(address(this).balance);
+  }
 }
--- scripts/deployNFT.ts
+++ scripts/deployNFT.ts
@@ -10,6 +10,13 @@ async function main() {
 
   const owner = await nft.owner();
   console.log("owner:", owner);
+
+  let txn = await nft.mintNFT(
+    "https://media.giphy.com/media/1gbqIc1fK8QgR3bHL7/giphy.gif"
+  );
+
+  const tokenIds = await nft._tokenIds();
+  console.log("tokenIds:", tokenIds);
 }
 
 main().catch((error) => {
