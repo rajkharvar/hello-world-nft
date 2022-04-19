7aa2ca65ce30ee533dc6e3657131f0c2cd97965f
rajkharvar <rajkharvar1808@gmail.com>
2022-04-20 00:58:00 +0530

    Initial setup for explore page

--- client/pages/explore.tsx
+++ client/pages/explore.tsx
@@ -1,21 +1,110 @@
-import { Heading } from "@chakra-ui/react";
-import { NextPage } from "next";
+/* eslint-disable node/no-unsupported-features/es-syntax */
 import { useQuery } from "@apollo/client";
-import { useEffect } from "react";
+import { Badge, Box, Flex, Image, Spinner, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { ethers } from "ethers";
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
 import { FETCH_NFTS } from "../utils/queries";
+
+type NFT = {
+  id: string;
+  onSale: boolean;
+  owner: string;
+  price: string;
+  tokenId: string;
+  tokenURI: string;
+  image: string;
+  title: string;
+  description: string;
+};
 
 const Explore: NextPage = () => {
   const { data, loading } = useQuery(FETCH_NFTS);
+  const [nfts, setNFTs] = useState<null | NFT[]>(null);
 
   useEffect(() => {
-    console.log("loading");
-    console.log(loading);
+    async function fetchNFTMetadata(nfts: NFT[]) {
+      const nftMetadata = await Promise.all(
+        nfts.map(async (nft) => {
+          const metadata = await axios.get(nft.tokenURI);
+          return {
+            ...nft,
+            ...metadata.data,
+          };
+        })
+      );
 
-    console.log("data");
-    console.log(data);
+      setNFTs(nftMetadata);
+    }
+
+    if (data?.nfts.length) {
+      fetchNFTMetadata(data.nfts);
+    }
   }, [data, loading]);
 
-  return <Heading>Explore</Heading>;
+  if (loading) {
+    return (
+      <Flex w="full" h="full" justifyContent="center" alignItems="center">
+        <Spinner size="lg" />
+      </Flex>
+    );
+  }
+
+  return (
+    <>
+      {nfts ? (
+        nfts.map((nft) => (
+          <Box
+            maxW="sm"
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            key={nft.id}
+            m={4}
+          >
+            <Image src={nft.image} alt={nft.image} />
+
+            <Box p="6">
+              <Box display="flex" alignItems="baseline">
+                <Badge borderRadius="full" px="2" colorScheme="teal">
+                  New
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  {nft.title}
+                </Box>
+              </Box>
+
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              >
+                {nft.description}
+              </Box>
+
+              <Box display="flex" mt="2" justifyContent="flex-end">
+                <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                  {ethers.utils.formatEther(nft.price)} ETH
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        ))
+      ) : (
+        <Text>No NFTs found</Text>
+      )}
+    </>
+  );
 };
 
 export default Explore;
